26b3d80972e949fa815986504bd4c459eea12022
Ana Carolina <carol00_10@hotmail.com>
2024-05-01 01:00:03 -0300

    arrumando o header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  searchQuery: string = ''; // Defina a variável searchQuery aqui se necessário
 
+  constructor(private bookService: BookService) {} // Injete o BookService se necessário
+
+  searchBooks() {
+    // Implemente a lógica de busca aqui, se necessário
+  }
 }
