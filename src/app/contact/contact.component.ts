1c1772f384bd0a439fa2e60a87ce59b0025fae0f
Ana Carolina <carol00_10@hotmail.com>
2024-05-02 15:53:17 -0300

    criando o salve no local storage dos dados

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  nome: string = 'The lord of the Books';
+  email: string = '';
+  contact: string = '';
+  message: string = '';
 
+  submitForm(): void {
+    // Verifica se os campos de entrada estão preenchidos
+    if (this.email) {
+      // Cria um objeto com as informações do formulário
+      const formData = {
+        nome: this.nome,
+        email: this.email,
+        contact: this.contact,
+        message: this.message
+      };
+
+      // Converte o objeto em uma string JSON
+      const formDataString = JSON.stringify(formData);
+
+      // Salva as informações no LocalStorage com uma chave única
+      localStorage.setItem('formData', formDataString);
+
+      // Limpa os campos de entrada após o envio do formulário
+      this.email = '';
+      this.contact = '';
+      this.message = '';
+
+      // Exibe uma mensagem de confirmação ou redireciona para outra página
+      alert('Formulário enviado com sucesso!');
+    } else {
+      // Exibe uma mensagem de erro se algum campo estiver vazio
+      alert('Por favor, preencha o campo de e-mail.');
+    }
+  }
 }
